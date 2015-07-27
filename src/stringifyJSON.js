bcb22c8ee9819037c5560a218676add3534990d6
John Wareing <jwareing89@gmail.com>
2015-07-27 12:06:05 -0500

    Fixed stringifyJSON.js. Used typeof and if statements to deal with simple cases, then for look with a couple of nested if statements to handle objects of interest (objects, arrays).

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	if (obj === null){
+		return "null";
+	}
+	else if (typeof obj === "undefined"){
+		return "undefined";
+	}
+	else if (typeof obj === "number"){
+		return obj.toString();
+	}
+	else if (typeof obj === "boolean"){
+		return obj.toString();
+	}
+	else if (typeof obj === "string"){
+		return '"'+obj+'"';
+	}
+	else{
+		var keyList = Object.keys(obj);
+		var stringParts = [];
+		var arrayParts = [];
+		for (var i=0;i<keyList.length;i++){
+			var keyVal = obj[keyList[i]];
+			var keyStr = "\""+keyList[i]+"\":";
+			if (typeof keyVal=="function" || typeof keyVal=="undefined"){
+				return '{}';
+			}
+			else{
+				keyStr = keyStr+stringifyJSON(keyVal);
+			}
+			arrayParts.push(stringifyJSON(keyVal));
+			stringParts.push(keyStr);
+		}
+	}
+	if (Array.isArray(obj)){
+		return "[" +arrayParts.join(",")+"]";
+	}
+	return "{" + stringParts.join(",") +"}";
 };
