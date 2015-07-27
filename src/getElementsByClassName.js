544bf2ecc2594dd7b35d13333c222f7273abef36
John Wareing <jwareing89@gmail.com>
2015-07-27 14:13:41 -0500

    Fixed getElementsByClassName.js. Added nodes whose class lists contained the desired class, and used recursion to look through all nodes on the page by checking children of all nodes.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,21 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//You should use document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+var getElementsByClassName = function(className){
+	var overall = document.body;
+	var nodeList = [];
+
+	function checker(element){
+		if (element.classList && element.classList.contains(className)){
+			nodeList.push(element);
+		}
+		for (var i=0;i<element.childNodes.length;i++){
+			checker(element.childNodes[i]);
+		}
+	}
+	checker(overall);
+	return nodeList;
+}
